d2ca44e0007f4b2d083505f7169e6205f5fcebae
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-12-08 07:11:22 -0500

    08 (#12)
    * Solve 08.a
    
    * Solve 08.b

---
+++ aoc_2022/day_08/a.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Mapping
+
+from aoc_2022.day_08.models import Point
+from aoc_2022.day_08.parser import Parser
+
+
+@dataclass
+class Day08PartASolver:
+    trees: Mapping[Point, int]
+
+    @property
+    def solution(self) -> int:
+        return len([p for p in self.trees.keys() if self.is_visible(p)])
+
+    def is_visible(self, point: Point) -> bool:
+        return self.trees[point] > min(
+            [
+                self.tallest_tree_to_down_map[point],
+                self.tallest_tree_to_up_map[point],
+                self.tallest_tree_to_left_map[point],
+                self.tallest_tree_to_right_map[point],
+            ]
+        )
+
+    @cached_property
+    def tallest_tree_to_left_map(self) -> Mapping[Point, int]:
+        output: dict[Point, int] = {}
+        for y in range(self.y_max + 1):
+            tallest_so_far = -1
+            for x in range(self.x_max + 1):
+                output[Point(x, y)] = tallest_so_far
+                tallest_so_far = max(tallest_so_far, self.trees[Point(x, y)])
+        return output
+
+    @cached_property
+    def tallest_tree_to_right_map(self) -> Mapping[Point, int]:
+        output: dict[Point, int] = {}
+        for y in range(self.y_max + 1):
+            tallest_so_far = -1
+            for x in range(self.x_max, -1, -1):
+                output[Point(x, y)] = tallest_so_far
+                tallest_so_far = max(tallest_so_far, self.trees[Point(x, y)])
+        return output
+
+    @cached_property
+    def tallest_tree_to_up_map(self) -> Mapping[Point, int]:
+        output: dict[Point, int] = {}
+        for x in range(self.x_max + 1):
+            tallest_so_far = -1
+            for y in range(self.y_max + 1):
+                output[Point(x, y)] = tallest_so_far
+                tallest_so_far = max(tallest_so_far, self.trees[Point(x, y)])
+        return output
+
+    @cached_property
+    def tallest_tree_to_down_map(self) -> Mapping[Point, int]:
+        output: dict[Point, int] = {}
+        for x in range(self.x_max + 1):
+            tallest_so_far = -1
+            for y in range(self.y_max, -1, -1):
+                output[Point(x, y)] = tallest_so_far
+                tallest_so_far = max(tallest_so_far, self.trees[Point(x, y)])
+        return output
+
+    @cached_property
+    def x_max(self) -> int:
+        return max([p.x for p in self.trees.keys()])
+
+    @cached_property
+    def y_max(self) -> int:
+        return max([p.y for p in self.trees.keys()])
+
+
+def solve(input: str) -> int:
+    data = Parser.parse(input)
+    solver = Day08PartASolver(data)
+
+    return solver.solution
+
+
+def get_solution() -> int:
+    with open("aoc_2022/day_08/input.txt", "r") as f:
+        input = f.read()
+    return solve(input)
+
+
+if __name__ == "__main__":
+    print(get_solution())
---
+++ aoc_2022/day_08/b.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Mapping
+
+from aoc_2022.day_08.models import Point
+from aoc_2022.day_08.parser import Parser
+
+
+@dataclass
+class Day08PartBSolver:
+    trees: Mapping[Point, int]
+
+    @property
+    def solution(self) -> int:
+        return max([self.scenic_score(p) for p in self.trees.keys()])
+
+    def scenic_score(self, p: Point) -> int:
+        height = self.trees[p]
+        left = 0
+        right = 0
+        up = 0
+        down = 0
+
+        for x in range(p.x - 1, -1, -1):
+            left += 1
+            if self.trees[Point(x, p.y)] >= height:
+                break
+        for x in range(p.x + 1, self.x_max + 1):
+            right += 1
+            if self.trees[Point(x, p.y)] >= height:
+                break
+        for y in range(p.y - 1, -1, -1):
+            up += 1
+            if self.trees[Point(p.x, y)] >= height:
+                break
+        for y in range(p.y + 1, self.y_max + 1):
+            down += 1
+            if self.trees[Point(p.x, y)] >= height:
+                break
+
+        return left * right * up * down
+
+    @cached_property
+    def x_max(self) -> int:
+        return max([p.x for p in self.trees.keys()])
+
+    @cached_property
+    def y_max(self) -> int:
+        return max([p.y for p in self.trees.keys()])
+
+
+def solve(input: str) -> int:
+    data = Parser.parse(input)
+    solver = Day08PartBSolver(data)
+
+    return solver.solution
+
+
+def get_solution() -> int:
+    with open("aoc_2022/day_08/input.txt", "r") as f:
+        input = f.read()
+    return solve(input)
+
+
+if __name__ == "__main__":
+    print(get_solution())

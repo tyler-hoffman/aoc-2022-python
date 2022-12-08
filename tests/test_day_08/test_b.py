4c3ef2f8de872c254abd75c5396a4a741f467f3e
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-12-08 07:07:50 -0500

    Solve 08.b

---
+++ tests/test_day_08/test_b.py
@@ -0,0 +1,29 @@
+import pytest
+
+from aoc_2022.day_08.b import Day08PartBSolver, get_solution
+from aoc_2022.day_08.models import Point
+from aoc_2022.day_08.parser import Parser
+
+SAMPLE_DATA = """
+30373
+25512
+65332
+33549
+35390
+"""
+
+
+@pytest.mark.parametrize(
+    "point, expected",
+    [
+        (Point(2, 1), 4),
+        (Point(2, 3), 8),
+    ],
+)
+def test_scenic_score(point: Point, expected: int):
+    solver = Day08PartBSolver(Parser.parse(SAMPLE_DATA))
+    assert solver.scenic_score(point) == expected
+
+
+def test_my_solution():
+    assert get_solution() == 410400

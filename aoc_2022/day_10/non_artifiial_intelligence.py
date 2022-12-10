52ecff437497b63d7cf6ab0b44771336fd50cfa4
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-12-10 18:06:59 -0500

    Minor update to 10

--- aoc_2022/day_10/non_artifiial_intelligence.py
+++ aoc_2022/day_10/non_artifiial_intelligence.py
@@ -3,19 +3,29 @@ class HumanService:
     from them as a string
     """
 
+    query_start_string = "[START QUERY]"
+    query_end_string = "[START QUERY]"
+
     def query(self, prompt: str) -> str:
+        print(self.get_start_line(prompt))
         print(prompt)
+        print(self.get_end_line(prompt))
+        print("answer plz: ", end="")
         return input()
 
+    def get_start_line(self, prompt: str) -> str:
+        return self.end_with_line(self.query_start_string, prompt)
 
-class HumanServiceFake(HumanService):
-    """Fake for HumanService.
-    Probably needed for CI unless I can figure out
-    how to send and recieve sms messages to/from CI.
-    """
+    def get_end_line(self, prompt: str) -> str:
+        return self.end_with_line(self.query_end_string, prompt)
 
-    def __init__(self, response: str) -> None:
-        self.response = response
+    def end_with_line(self, start: str, prompt: str) -> str:
+        start = self.query_start_string + " "
+        desired_len = self.get_prompt_length(prompt)
+        start_len = len(start)
+        return start + "=" * (desired_len - start_len)
 
-    def query(self, _: str) -> str:
-        return self.response
+    def get_prompt_length(self, prompt: str) -> int:
+        lines = prompt.split("\n")
+        lengths = [len(line) for line in lines]
+        return max(lengths)
